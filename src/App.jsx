5475b0813f2e5f49d5fb3dce19be61b13fc6f8d8
Fr33yr <75025925+Fr33yr@users.noreply.github.com>
2022-03-15 18:10:01 -0300

    Remarkable implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,61 +1,58 @@
-import React,{Fragment} from "react";
+import React,{Fragment, useState} from "react";
+import { Remarkable } from "remarkable";
 import './App.css'
 
 
+const md = new Remarkable();
+
+const content = `
+
+# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+**This is bolded text**
+> Block Quotes!
+
+-list item 1
+-list item 2
+-list item 3
+
+[Visit my website](#)
+
+This is a inline \'<div></div>\'
+
+
+This is a block of code 
+
+\'\'\'
+while (i < 10) {
+  text += "The number is " + i;
+  i++;
+}
+\'\'\'
+
+![React](https://upload.wikimedia.org/wikipedia/commons/thumb/4/47/React.svg/1200px-React.svg.png)
+
+`;
+
 export function App(){
+
+  const [text, setText] = useState(content);
 
   return(
 
     <Fragment>
-
-      <div className="editor-container">
-        <div className="editor-toolbar">Toolbar</div>
-        <textarea id="editor" type="text">asd</textarea>
-      </div>
-      <div className="converter"></div>
-      <div className="previewer-container">
-        <div className="previewer-toolbar">Toolbar</div>
-        <div id="preview">
-          <h1>Here goes the title</h1>
-          <h2>This is a sub title</h2>
-          <h3>And here goes other cool stuff</h3>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus, repellendus.</p>
-          <pre>
-            <code>
-              {/* some code goes here */}
-            </code>
-          </pre>
-          <p>
-            Lorem ipsum dolor
-            <br/>sit amet.
-            <br/>sit amet.
-            <br/>sit amet. 
-          </p>
-          <p>There also <a href="#">Links</a></p>
-          <blockquote>Blockquotes!</blockquote>
-          <p>And if you like some tables to.</p>
-          <ul>
-            <li>And of course there lists
-              <ul>
-                <li>some are bulleted.
-                  <ul>
-                    <li>With diferent identation levels
-                      <ul>
-                        <li>That looks like this.</li>
-                      </ul>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </li>
-          </ul>
-          <ul>
-            <li>1.And there are numbered lists too.</li>
-            <li>2.And there are numbered lists too.</li>
-          </ul>
-          <img src="" alt="" />
+        <div className="editor-container">
+          <div className="editor-toolbar">Toolbar</div>
+          <textarea name="textarea" id="editor" value={text} onChange={(e) => setText(e.target.value)} cols="30" rows="10"></textarea>
         </div>
-      </div>
+
+        <div className="previewer-container">
+          <div className="previewer-toolbar">Toolbar</div>
+          <div className="previewer" id="preview" dangerouslySetInnerHTML={{ __html: md.render(text) }}></div>
+        </div>
     </Fragment>
   )
 }
